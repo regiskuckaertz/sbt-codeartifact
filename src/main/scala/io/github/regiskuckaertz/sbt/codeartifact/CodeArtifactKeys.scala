01b51ed039d919be3ffe3ac46b2e6af05df795ca
Regis Kuckaertz <regkz@hey.com>
2026-07-31 12:25:30 +0100

    Make settings non-optional and more precise
    It is annoying to have to wrap setting values in `Some` when they are
    always going to be provided. Adding a default value that will blow
    up visibly instead is just more than enough. Furthermore, we have
    types at our disposal to better enforce correctness, e.g. for the
    `codeArtifactRegion` setting.
    
    Lastly, I added the ability to change how often the token is valid,
    as some internal security policies might impose stricter rules.

--- src/main/scala/io/github/regiskuckaertz/sbt/codeartifact/CodeArtifactKeys.scala
+++ src/main/scala/io/github/regiskuckaertz/sbt/codeartifact/CodeArtifactKeys.scala
@@ -1,11 +1,14 @@
 package io.github.regiskuckaertz.sbt.codeartifact
 
+import scala.concurrent.duration.FiniteDuration
 import sbt.*
+import software.amazon.awssdk.regions.Region
 
 trait CodeArtifactKeys {
-  val codeArtifactDomain:      SettingKey[Option[String]]
-  val codeArtifactDomainOwner: SettingKey[Option[String]]
-  val codeArtifactRegion:      SettingKey[Option[String]]
-  val codeArtifactRepository:  SettingKey[Option[String]]
+  val codeArtifactDomain:        SettingKey[String]
+  val codeArtifactDomainOwner:   SettingKey[String]
+  val codeArtifactRegion:        SettingKey[Region]
+  val codeArtifactRepository:    SettingKey[String]
+  val codeArtifactTokenDuration: SettingKey[FiniteDuration]
   val codeArtifactToken:       TaskKey[Option[String]]
 }
